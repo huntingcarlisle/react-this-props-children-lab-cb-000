f772e66373043929659c7ba66243c2bf66170ee6
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-26 23:21:43 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -6,8 +6,7 @@ export default class ThemedDecorations extends React.Component {
   render() {
     return (
       <div className='themed-decorations'>
-        <div>{this.props.children}</div>
-      </div>
+        <div>{this.props.children}
       )
   }
 }
