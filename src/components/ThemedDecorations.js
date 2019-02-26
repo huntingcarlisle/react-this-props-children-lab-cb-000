4b656fd01089743c0937b8020e86f0f018422cba
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-26 23:14:23 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,9 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ThemedDecoration extends Reaact.Component {
+  render() {
+    return ()
+  }
+}
