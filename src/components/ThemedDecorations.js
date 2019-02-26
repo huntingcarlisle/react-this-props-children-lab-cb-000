42c9d59fa0affcf06616fb32688a46f1d16729e9
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-26 23:21:48 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -5,8 +5,7 @@ import ReactDOM from 'react-dom';
 export default class ThemedDecorations extends React.Component {
   render() {
     return (
-      <div className='themed-decorations'>
-        <div>{this.props.children}
+      {this.props.children}
       )
   }
 }
