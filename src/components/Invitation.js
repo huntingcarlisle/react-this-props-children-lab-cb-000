8cab4f5f30d2a52c78c79ad456c5de233eea32fe
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-26 23:17:27 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 export default class Invitation extends React.Component {
   render() {
     return (
-      <h1>Title</h1>
+      <h1>You've been invited!</h1>
       )
   }
 }
